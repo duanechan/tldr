30d9320709b2d2ff385852bff37e57a6b797fcc5
Duane Chan <duanechan19@gmail.com>
2026-05-11 10:36:53 +0800

    feat: Add auth package

--- go.mod
+++ go.mod
@@ -18,6 +18,7 @@ require (
 	github.com/felixge/httpsnoop v1.0.4 // indirect
 	github.com/go-logr/logr v1.4.3 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/golang-jwt/jwt/v5 v5.3.1 // indirect
 	github.com/golang/groupcache v0.0.0-20241129210726-2c02b8208cf8 // indirect
 	github.com/google/go-cmp v0.7.0 // indirect
 	github.com/google/s2a-go v0.1.9 // indirect
---
+++ internal/auth/token.go
@@ -0,0 +1,52 @@
+package auth
+
+import (
+	"errors"
+	"net/http"
+	"strings"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
+)
+
+func GetBearerToken(headers http.Header) (string, error) {
+	bearerPrefix := "Bearer "
+	auth := headers.Get("Authorization")
+	if !strings.HasPrefix(auth, bearerPrefix) {
+		return "", errors.New("Invalid/malformed authorization header")
+	}
+
+	token := auth[len(bearerPrefix):]
+	return token, nil
+}
+
+func CreateJWT(id uuid.UUID, username, tokenSecret string, expiresIn time.Duration) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.RegisteredClaims{
+		Issuer:    "tldr",
+		ExpiresAt: jwt.NewNumericDate(time.Now().Add(expiresIn)),
+		Subject:   id.String(),
+	})
+
+	signed, err := token.SignedString([]byte(tokenSecret))
+	if err != nil {
+		return "", err
+	}
+
+	return signed, nil
+}
+
+func ValidateJWT(tokenString, tokenSecret string) (jwt.Claims, error) {
+	token, err := jwt.ParseWithClaims(
+		tokenString,
+		&jwt.RegisteredClaims{Issuer: "tldr"},
+		func(t *jwt.Token) (any, error) {
+			return []byte(tokenSecret), nil
+		},
+		jwt.WithIssuer("tldr"))
+	if err != nil {
+		return nil, err
+	}
+
+	return token.Claims, nil
+}
