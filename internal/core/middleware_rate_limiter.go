3a7bd9728b4901be3374b0202960ac88ac2e2c2a
Duane Chan <duanechan19@gmail.com>
2026-05-14 12:40:01 +0800

    refactor: Move locks to limiter block

--- internal/core/middleware_rate_limiter.go
+++ internal/core/middleware_rate_limiter.go
@@ -15,19 +15,19 @@ const (
 
 func (t *TLDR) RateLimiterMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		t.mu.Lock()
-		defer t.mu.Unlock()
 		host, _, err := net.SplitHostPort(r.RemoteAddr)
 		if err != nil {
 			t.errorResponse(w, r.Context(), http.StatusBadRequest, "Invalid IP address")
 			return
 		}
 
+		t.mu.Lock()
 		limiter, exists := t.clients[host]
 		if !exists {
 			limiter = rate.NewLimiter(rate.Every(rateLimitInterval), burstLimit)
 			t.clients[host] = limiter
 		}
+		t.mu.Unlock()
 
 		if !limiter.Allow() {
 			t.errorResponse(w, r.Context(), http.StatusTooManyRequests, "Rate limit exceeded, try again later")
