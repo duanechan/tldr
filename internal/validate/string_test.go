9e7f834573ab265b9d4ba78c12cd8b56b420dc96
Duane Chan <duanechan19@gmail.com>
2026-05-22 00:32:41 +0800

    feat: Add unit tests for validate.String

--- internal/validate/string_test.go
+++ internal/validate/string_test.go
@@ -1,7 +1,120 @@
 package validate
 
-import "testing"
+import (
+	"testing"
+)
 
-func TestString(t *testing.T) {
+func TestString_NoOptions(t *testing.T) {
+	var tests = []struct {
+		name  string
+		input string
+	}{
+		{
+			name:  "String with length",
+			input: "hello, world!",
+		},
+		{
+			name:  "Empty string",
+			input: "",
+		},
+		{
+			name:  "String with only whitespace",
+			input: "     ",
+		},
+	}
 
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			actual, err := String(tt.input)
+			if err != nil {
+				t.Fatalf("expected error to be nil, got %v", err.Error())
+			}
+
+			if tt.input != actual {
+				t.Fatalf("want %s, got %s", tt.input, actual)
+			}
+		})
+	}
+}
+
+func TestString_WithOptions(t *testing.T) {
+	var tests = []struct {
+		wantErr  bool
+		name     string
+		input    string
+		expected string
+		fn       func(string) (string, error)
+	}{
+		{
+			wantErr:  false,
+			name:     "String within range",
+			input:    "Hello!",
+			expected: "Hello!",
+			fn: func(s string) (string, error) {
+				return String(s, Range(3, 8))
+			},
+		},
+		{
+			wantErr:  true,
+			name:     "String exceeding range",
+			input:    "Hello, world!",
+			expected: "",
+			fn: func(s string) (string, error) {
+				return String(s, Range(3, 8))
+			},
+		},
+		{
+			wantErr:  false,
+			name:     "String within minimum limit",
+			input:    "yes",
+			expected: "yes",
+			fn: func(s string) (string, error) {
+				return String(s, Min(3))
+			},
+		},
+		{
+			wantErr:  true,
+			name:     "String preceding minimum limit",
+			input:    "no",
+			expected: "",
+			fn: func(s string) (string, error) {
+				return String(s, Min(3))
+			},
+		},
+		{
+			wantErr:  false,
+			name:     "String within maximum limit",
+			input:    "Hello!",
+			expected: "Hello!",
+			fn: func(s string) (string, error) {
+				return String(s, Max(6))
+			},
+		},
+		{
+			wantErr:  true,
+			name:     "String exceeding maximum limit",
+			input:    "Hello, world!",
+			expected: "",
+			fn: func(s string) (string, error) {
+				return String(s, Max(6))
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			actual, err := tt.fn(tt.input)
+			if !tt.wantErr && err != nil {
+				t.Fatalf("expected error to be nil, got %v", err.Error())
+			}
+
+			if tt.wantErr && err == nil {
+				t.Fatal("expected error to be non-nil")
+			}
+
+			if tt.expected != actual {
+				t.Fatalf("want %v, got %v", tt.expected, actual)
+			}
+		})
+	}
 }
