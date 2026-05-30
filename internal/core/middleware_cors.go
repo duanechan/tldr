e56fdd094180c18e7ab04f23dc43687edce78df1
Duane Chan <duanechan19@gmail.com>
2026-05-31 02:12:26 +0800

    fix: Add preflight requests to cors middleware

--- internal/core/middleware_cors.go
+++ internal/core/middleware_cors.go
@@ -5,6 +5,12 @@ import "net/http"
 func CorsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
+		if r.Method == http.MethodOptions {
+			w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+			w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+			w.WriteHeader(http.StatusNoContent)
+			return
+		}
 		next.ServeHTTP(w, r)
 	})
 }
